5509c84822ba11d8877a0a1ccff3040427ada9b5
Robert Moegenburg <r.moegenburg@t-online.de>
2024-05-18 22:30:26 +0200

    123

--- scenes/Controller/Car_Controller.gd
+++ scenes/Controller/Car_Controller.gd
@@ -1,34 +1,54 @@
 extends VehicleBody3D
-
-var isJumping: bool = false
-var isBoosting: bool = false
-
-var current_steering : float = 25
+enum Gamestate {
+	FLYMODE,
+	RACEMODE
+}
+var current_game_state : Gamestate = Gamestate.RACEMODE
+var is_Jumping: bool = false
+var is_Boosting: bool = false
+var is_rolling : bool = false
+var current_boost : float = 1
+var current_steering : float = 25.0
 var boost_amount : int = 100
-const JUMPOWER : float = 0.3
-const BOOSTPOWER : float = 1.1
-const MAX_STEERING :float = 25
-const MAX_STEERING_HANDBRAKE :float = 50
+const JUMPOWER : float = 6.0
+const BOOSTPOWER : float = 1.5
+const MAX_STEERING :float = 25.0
+const MAX_STEERING_HANDBRAKE :float = 50.0
 const ENGINE_POWER : float = 5000
-const BRAKE_POWER : float = 2500
+const BRAKE_POWER : float = 5000
+const ROLL_SPEED: float = 20.0
+const PITCH_SPEED: float  = 8.0
+const ANGULAR_VELOCITY_DECAY : float = 0.96
+const YAW_SPEED = 8.0
+
 var move_direction : Vector2  =Vector2.ZERO
-@export var slip_speed = 9.0
 @export var friction_slip : Vector2 = Vector2 (0.02,0.845)
 @export var wheel_rear_left : VehicleWheel3D
 @export var wheel_rear_right : VehicleWheel3D
-
-var drifting = false
-
+@export var light_brake_left : OmniLight3D
+@export var light_brake_right : OmniLight3D
+var current_jumps = 0
+const MAX_JUMPS : int = 2
 func _input(_event):
 	move_direction = Input.get_vector("move_right", "move_left","brake", "accellerate")
+	################ JUMP
 	if _event.is_action_pressed("jump"):
-		isJumping =true
+		is_Jumping =true
 	if _event.is_action_released("jump"):
-		isJumping =false
+		is_Jumping =false
+	#################### ROLL
+	if _event.is_action_pressed("roll"):
+		is_rolling =true
+	if _event.is_action_released("roll"):
+		is_rolling =false
+	################### BOOST 
 	if _event.is_action_pressed("boost"):
-		isBoosting =true
+		is_Boosting =true
+		current_boost = BOOSTPOWER
 	if _event.is_action_released("boost"):
-		isBoosting =false
+		is_Boosting =false
+		current_boost = 1
+	############################ HANDBRAKE
 	if _event.is_action_pressed("handbrake"):
 		current_steering = MAX_STEERING_HANDBRAKE
 		wheel_rear_left.wheel_friction_slip = friction_slip.x
@@ -44,30 +64,37 @@ func _physics_process(_delta):
 	handle_Acceleration()
 	handle_Steering()
 	handle_Jump()
-	handle_Boost()
-	handle_drift()
-
+	if current_game_state == Gamestate.FLYMODE:
+		handle_flying()
 
 func handle_Acceleration():
-	engine_force = move_direction.y *ENGINE_POWER
+	engine_force = move_direction.y *ENGINE_POWER *current_boost
 	brake = move_direction.y  * BRAKE_POWER
+	if brake > 5:
+		light_brake_left.light_energy = 6
+		light_brake_right.light_energy = 6
+	if brake < 5:
+		light_brake_left.light_energy = 1
+		light_brake_right.light_energy = 1
 func handle_Steering():
 	steering = deg_to_rad(move_direction.x * current_steering)
 func handle_Jump():
-	if(isJumping):
-		global_position += Vector3.UP * JUMPOWER
+	if(is_Jumping and current_jumps < MAX_JUMPS):
+		#reset on ground
+		current_game_state = Gamestate.FLYMODE
+		current_jumps += 1
+		linear_velocity += Vector3.UP * JUMPOWER
 		global_rotation = Vector3.ZERO
+	if linear_velocity.y <1:
+		current_jumps = 0
+		current_game_state = Gamestate.RACEMODE
 
-func handle_Boost():
-	if (isBoosting):
-		engine_force = move_direction.y * BOOSTPOWER
+func handle_flying():
+	add_constant_torque(Vector3.RIGHT * PITCH_SPEED * move_direction.y)
 
-func handle_drift():
-	pass
-	# # traction
-	# if not drifting and angular_velocity.length() > slip_speed:
-	# 	drifting = true
-	# if drifting and angular_velocity.length() < slip_speed and steering == 0:
-	# 	drifting = false
-	# var traction = traction_fast if drifting else traction_slow
-	# angular_velocity = lerp(angular_velocity, angular_velocity.length(), traction)
+	if(is_rolling):
+		add_constant_torque(Vector3.FORWARD * ROLL_SPEED * -move_direction.x)
+	else:
+		add_constant_torque(Vector3.UP * YAW_SPEED * move_direction.x)
+
+	angular_velocity *= ANGULAR_VELOCITY_DECAY
